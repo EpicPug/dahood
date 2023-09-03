d8285bde19b4c5dee140f1699d08b7aa78ec4df1
EpicPug <112693990+EpicPug@users.noreply.github.com>
2023-09-03 09:11:15 -0500

    Update MobleToggle.lua

--- Features/MobleToggle.lua
+++ Features/MobleToggle.lua
@@ -1,74 +1,82 @@
 -- This Script Is For Moble Users To able To Toggle The Gui.
 -- And Yes Its source Take The Code If You Want.
 
-if game.PlaceId == 2788229376 and not game.CoreGui:FindFirstChild("MobleToggleButton") and game.CoreGui:FindFirstChild("Auto Farm V1.2") then
-	local Gui = Instance.new("ScreenGui")
-	local UIListLayout = Instance.new("UIListLayout")
-	local Frame = Instance.new("Frame")
-	local UIListLayout_2 = Instance.new("UIListLayout")
-	local Toggle = Instance.new("TextButton")
-	local UICorner = Instance.new("UICorner")
-	
-	--Properties:
+local PlaceId = 2788229376
 
-	Gui.Name = "MobleToggleButton"
-	Gui.Parent = game.CoreGui
-	Gui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-
-	UIListLayout.Parent = Gui
-	UIListLayout.SortOrder = Enum.SortOrder.LayoutOrder
-	UIListLayout.VerticalAlignment = Enum.VerticalAlignment.Center
-
-	Frame.Parent = Gui
-	Frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-	Frame.BackgroundTransparency = 1.000
-	Frame.BorderColor3 = Color3.fromRGB(0, 0, 0)
-	Frame.BorderSizePixel = 0
-	Frame.Position = UDim2.new(0, 0, 0.490123451, 0)
-	Frame.Size = UDim2.new(0, 95, 0, 16)
-
-	UIListLayout_2.Parent = Frame
-	UIListLayout_2.HorizontalAlignment = Enum.HorizontalAlignment.Right
-	UIListLayout_2.SortOrder = Enum.SortOrder.LayoutOrder
-	UIListLayout_2.VerticalAlignment = Enum.VerticalAlignment.Center
-
-	Toggle.Name = "Toggle"
-	Toggle.Parent = Frame
-	Toggle.BackgroundColor3 = Color3.fromRGB(22, 22, 22)
-	Toggle.BorderColor3 = Color3.fromRGB(0, 0, 0)
-	Toggle.BorderSizePixel = 0
-	Toggle.Position = UDim2.new(0, 0, 0.482716054, 0)
-	Toggle.Size = UDim2.new(0, 87, 0, 27)
-	Toggle.Font = Enum.Font.FredokaOne
-	Toggle.Text = "Close"
-	Toggle.TextColor3 = Color3.fromRGB(255, 255, 255)
-	Toggle.TextSize = 14.000
-
-	UICorner.Parent = Toggle
-	
-	local Players = game:GetService("Players")
-	local Player = Players.LocalPlayer
-	
-	local TweenService = game:GetService("TweenService")
-	
-	local Gui = game:GetService("CoreGui"):FindFirstChild("Auto Farm V1.2")
-	
-	Toggle.MouseButton1Click:Connect(function()
-		if Gui == nil then Player:Kick("Error: 203") return end
-		if Toggle.Text == "Close" then
-			Toggle.Text = "Open"
-			Gui:FindFirstChild("Main").Visible = false
-		else
-			Toggle.Text = "Close"
-			Gui:FindFirstChild("Main").Visible = true
-		end
-	end)
-	
-	Toggle.MouseEnter:Connect(function()
-		Toggle.TextColor3 = Color3.fromRGB(255, 255, 255)
-	end)
-	
-	Toggle.MouseLeave:Connect(function()
-		Toggle.TextColor3 = Color3.fromRGB(150, 150, 150)
-	end)
+if game.PlaceId ~= PlaceId then
+	return
 end
+
+if game.CoreGui:FindFirstChild("MobleToggleButton") or not game.CoreGui:FindFirstChild("Auto Farm V1.2") then
+	return
+end
+
+local MobleToggleButton = Instance.new("ScreenGui")
+local UIListLayout = Instance.new("UIListLayout")
+local Frame = Instance.new("Frame")
+local Button = Instance.new("TextButton")
+local UICorner = Instance.new("UICorner")
+local UIListLayout_2 = Instance.new("UIListLayout")
+
+--Properties:
+
+MobleToggleButton.Name = "MobleToggleButton"
+MobleToggleButton.Parent = game.CoreGui
+MobleToggleButton.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+UIListLayout.Parent = MobleToggleButton
+UIListLayout.SortOrder = Enum.SortOrder.LayoutOrder
+UIListLayout.VerticalAlignment = Enum.VerticalAlignment.Center
+
+Frame.Parent = MobleToggleButton
+Frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Frame.BackgroundTransparency = 1.000
+Frame.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Frame.BorderSizePixel = 0
+Frame.Position = UDim2.new(0, 0, 0.498765439, 0)
+Frame.Size = UDim2.new(0, 101, 0, 6)
+
+Button.Name = "Button"
+Button.Parent = Frame
+Button.BackgroundColor3 = Color3.fromRGB(24, 24, 24)
+Button.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Button.BorderSizePixel = 0
+Button.Position = UDim2.new(0.546341479, 0, -20, 0)
+Button.Size = UDim2.new(0, 93, 0, 31)
+Button.Font = Enum.Font.FredokaOne
+Button.Text = "Close"
+Button.TextColor3 = Color3.fromRGB(116, 116, 116)
+Button.TextSize = 15.000
+
+UICorner.CornerRadius = UDim.new(0, 6)
+UICorner.Parent = Button
+
+UIListLayout_2.Parent = Frame
+UIListLayout_2.HorizontalAlignment = Enum.HorizontalAlignment.Right
+UIListLayout_2.SortOrder = Enum.SortOrder.LayoutOrder
+UIListLayout_2.VerticalAlignment = Enum.VerticalAlignment.Center
+
+local Player = game:GetService("Players").LocalPlayer
+
+local TweenService = game:GetService("TweenService")
+
+local Gui = game:GetService("CoreGui"):FindFirstChild("Auto Farm V1.2")
+
+Button.MouseButton1Click:Connect(function()
+	if Gui == nil then Player:Kick("Error") return end
+	if Button.Text == "Close" then
+		Button.Text = "Open"
+		Gui:FindFirstChild("Main").Visible = false
+	else
+		Button.Text = "Close"
+		Gui:FindFirstChild("Main").Visible = true
+	end
+end)
+
+Button.MouseEnter:Connect(function()
+	Button.TextColor3 = Color3.fromRGB(255, 255, 255)
+end)
+
+Button.MouseLeave:Connect(function()
+	Button.TextColor3 = Color3.fromRGB(116, 116, 116)
+end)
