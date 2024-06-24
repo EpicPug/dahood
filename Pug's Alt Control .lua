11c2439358010b2b56f7baa8d7244ac42308a4ab
EpicPug <112693990+EpicPug@users.noreply.github.com>
2024-06-24 14:38:23 -0700

    Update Pug's Alt Control .lua

--- Pug's Alt Control .lua
+++ Pug's Alt Control .lua
@@ -1,1 +1,30 @@
-return 'nil lol'
+if not game:IsLoaded() then
+	game.Loaded:Wait()
+end
+
+local Place_Id = game.PlaceId
+local Target_PlaceId = 2788229376
+
+if Place_Id == Target_PlaceId then
+	print('Correct PlaceId')
+	print('Now loading!')
+else
+	print('Incorrect PlaceId. This script is intended for another game instance.')
+	return
+end
+
+if game.CoreGui:FindFirstChild('alt core?') or game.CoreGui:FindFirstChild('host core?') then
+	return
+end
+
+repeat wait(1) until game.Players.LocalPlayer and game.Players.LocalPlayer.Character and game.Players.LocalPlayer:FindFirstChild('DataFolder')
+
+pcall(function()loadstring(game:HttpGet('https://github.com/EpicPug/Stuff/blob/main/AntiAfk.lua'))()end)
+pcall(function()loadstring(game:HttpGet('https://raw.githubusercontent.com/EpicPug/Stuff/main/AntiSit.lua'))()end)
+pcall(function()loadstring(game:HttpGet('https://raw.githubusercontent.com/EpicPug/Stuff/main/LowGfx.lua'))()end)
+
+pcall(function()
+	if tonumber(getgenv().Fps) then
+		setfpscap(getgenv().Fps)
+	end
+end)
