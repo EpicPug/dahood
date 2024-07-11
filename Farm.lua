c48e2848e091677fcfd397536ee5b0ead09eca0c
EpicPug <112693990+EpicPug@users.noreply.github.com>
2024-07-10 18:01:13 -0700

    Update Farm.lua

--- Farm.lua
+++ Farm.lua
@@ -12,13 +12,26 @@ end
 
 repeat task.wait(1) until game.Players.LocalPlayer and game.Players.LocalPlayer.Character and game.Players.LocalPlayer:FindFirstChild('DataFolder')
 
-pcall(function()loadstring(game:HttpGet('https://raw.githubusercontent.com/EpicPug/Stuff/main/AntiAfk.lua'))()end)
+pcall(function()
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/EpicPug/Stuff/main/AntiAfk.lua'))()
+end)
 
-pcall(function()loadstring(game:HttpGet('https://raw.githubusercontent.com/EpicPug/Stuff/main/AntiSit.lua'))()end)
+pcall(function()
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/EpicPug/Stuff/main/AntiSit.lua'))()
+end)
 
-pcall(function()setfpscap(_G.Config.Fps)end)
+pcall(function()
+	setfpscap(_G.Config.Fps)
+end)
 
 _G.SideConfig = {
-	Key = {Name = '[Key] - $133', Price = 133, Tool = '[Key]'},
-	Pos = {4,-3.9}
+	Key = {
+		Name = '[Key] - $133',
+		Price = 133,
+		Tool = '[Key]'
+	},
+	
+	Pos = {
+		4, -3.9
+	}
 }
