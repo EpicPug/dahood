4609bcaf6c85c79dbd3b7fa498351ae3b57e2a90
EpicPug <112693990+EpicPug@users.noreply.github.com>
2024-07-17 08:06:59 -0700

    Update Farm.lua

--- Farm.lua
+++ Farm.lua
@@ -1,4 +1,4 @@
-if game:IsLoaded() == false then
+if not game:IsLoaded() then
 	game.Loaded:Wait()
 end
 
@@ -10,7 +10,7 @@ if game.CoreGui:FindFirstChild('core?') then
 	return
 end
 
-repeat task.wait(1) until game.Players.LocalPlayer and game.Players.LocalPlayer.Character and game.Players.LocalPlayer:FindFirstChild('DataFolder')
+repeat task.wait(1) until game.Players.LocalPlayer and game.Players.LocalPlayer:FindFirstChild('DataFolder') and game.Players.LocalPlayer.Character
 
 pcall(function()
 	loadstring(game:HttpGet('https://raw.githubusercontent.com/EpicPug/Stuff/main/AntiAfk.lua'))()
@@ -21,10 +21,14 @@ pcall(function()
 end)
 
 pcall(function()
-	setfpscap(getgenv().config["Fps"])
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/EpicPug/Stuff/main/Low.lua'))()
 end)
 
-getgenv().sideconfig = {
+pcall(function()
+	setfpscap(getgenv().Settings["Fps"])
+end)
+
+getgenv().Config = {
 	["Mask"] = {
 		Name = '[Surgeon Mask] - $27',
 		Price = 27,
