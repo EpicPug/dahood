e5d133fa7a547dc102a482b5f2d1a336d01f4ec7
EpicPug <112693990+EpicPug@users.noreply.github.com>
2026-05-26 19:19:28 -0500

    Update AutoFarm.lua

--- AutoFarm.lua
+++ AutoFarm.lua
@@ -1,2 +1,13 @@
-local Players = game:GetService("Players")
-Players.LocalPlayer:Kick(":(")
+--!strict
+
+const Players = game:GetService("Players")
+
+const player = Players.LocalPlayer :: Player
+
+const MESSAGE = "This script is no longer available."
+
+if not game:IsLoaded() then
+	game.Loaded:Wait()
+end
+
+pcall(player.Kick, player, MESSAGE)
